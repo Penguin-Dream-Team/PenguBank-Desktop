429ca97d804c32ac1a6771bf34eb09bed5e0eb3f
João Soares <jsoares.public@gmail.com>
2020-11-17 18:40:21 +0000

    Init Desktop App

--- src/PenguBankApplication.kt
+++ src/PenguBankApplication.kt
@@ -1,4 +1,15 @@
+import javafx.stage.Stage
 import tornadofx.App
+import tornadofx.launch
 import view.MainView
 
-class PenguBankApplication: App(MainView::class, Styles::class)
+class PenguBankApplication: App(MainView::class, Styles::class) {
+    override fun start(stage: Stage) {
+        with(stage) {
+            isResizable = false
+        }
+        super.start(stage)
+    }
+}
+
+fun main() = launch<PenguBankApplication>()
--- src/view/MainView.kt
+++ src/view/MainView.kt
@@ -4,7 +4,11 @@ import Styles
 import tornadofx.*
 
 class MainView : View("Hello TornadoFX") {
+
     override val root = hbox {
+        prefWidth = 1280.0
+        prefHeight = 720.0
+
         label(title) {
             addClass(Styles.heading)
         }
